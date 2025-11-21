5c4592713fca9c1e3051fbab60a0cfbb0517590a
gilletth-dev <gilletthibault23@gmail.com>
2025-11-21 04:22:33 +0100

    0.4

--- assets/app.js
+++ assets/app.js
@@ -1,1 +1,29 @@
 "use strict"; 
+
+// BackToTop
+const backToTop = document.querySelector(".backToTop");
+const target = document.getElementById("top");
+
+if(backToTop && target){
+window.addEventListener("scroll", showBackToTop);
+backToTop.addEventListener("click", smoothScroll);
+}
+// console.log(window.innerHeight);
+function showBackToTop(){
+    const currentScroll = window.scrollY;
+    if(currentScroll > window.innerHeight/4){
+        backToTop.classList.add("backToTop--show");
+        // console.log(currentScroll);
+    }
+    else{
+        backToTop.classList.remove("backToTop--show");
+    }
+}
+
+function smoothScroll(event){
+    console.log("smooth")
+    event.preventDefault();
+    target.scrollIntoView({behavior: "smooth"});
+}
+
+
